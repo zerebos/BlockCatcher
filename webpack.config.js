bea2ecdb7268f46d40e23658cad014775baf9b9e
Zack Rauen <rauenzi@outlook.com>
2023-07-28 19:14:53 -0400

    Fix for alternate environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.argv.some(a => a === "production");
 
 /* eslint-disable quote-props */
 
